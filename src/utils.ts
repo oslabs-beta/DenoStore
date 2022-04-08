d880daa67c824ca87e0330bfea37b83854e4bcff
James Kim <james.minjae.97@gmail.com>
2022-04-07 22:04:03 -0400

    moved all typing to types.ts and to have all types in a centralized area

--- src/utils.ts
+++ src/utils.ts
@@ -1,33 +1,5 @@
 import { QueryObjType, GraphQLResolveInfo } from './types.ts';
 
-// const queryParser = (queryString: string): string => {
-//   if (queryString === '') {
-//     console.error(
-//       '%cError finding query in the resolver.',
-//       'font-weight: bold; color: white; background-color: red;'
-//     );
-//     throw new Error('Query error. See server console.');
-//   }
-//   if (queryString[0] === 'm') {
-//     console.error(
-//       '%cDenostore cache function does not allow caching of mutations.',
-//       'font-weight: bold; color: white; background-color: red;'
-//     );
-//     throw new Error('Query error. See server console.');
-//   }
-//   let queryName = '';
-//   let curlyCount = 0;
-
-//   for (let i = 0; i < queryString.length && curlyCount < 2; i++) {
-//     if (queryString[i] === '{') curlyCount++;
-//     if (curlyCount === 1 && queryString[i] !== '{') {
-//       queryName += queryString[i];
-//     }
-//   }
-//   queryName = queryName.trim();
-//   return queryName;
-// };
-
 const queryExtract = (info: GraphQLResolveInfo): string => {
   if (info.operation.operation === 'mutation') {
     console.error(
