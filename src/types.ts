22ed8682aaeafd1341e87ed04d50b6a26fe4a1ee
scott <sktatsuno@gmail.com>
2022-04-16 14:28:13 -0700

    merging denostore argument changes to dev

--- src/types.ts
+++ src/types.ts
@@ -4,25 +4,67 @@ import type {
   GraphQLResolveInfo,
   FieldNode,
   ArgumentNode,
+  DocumentNode,
+  DefinitionNode,
+  Source,
 } from 'https://deno.land/x/graphql_deno@v15.0.0/mod.ts';
 import type {
   Middleware,
   Context,
 } from 'https://deno.land/x/oak@v10.2.0/mod.ts';
+import type { IResolvers } from 'https://deno.land/x/graphql_tools@0.0.2/utils/interfaces.ts';
 
 export interface DenostoreArgs {
-  schema: GraphQLSchema;
-  redisClient: Redis;
+  schema: userSchemaArg;
+  redisClient?: Redis;
+  redisPort?: number;
   route?: string;
   usePlayground?: boolean;
   defaultEx?: number | undefined;
 }
 
-type Maybe<T> = null | undefined | T;
-export interface QueryObjType {
+export type defaultExArg = number | undefined;
+
+export type redisClientArg = Redis | undefined;
+
+export type redisPortArg = number | undefined;
+
+export type userSchemaArg = GraphQLSchema | ExecutableSchemaArgs;
+
+export type cacheCallbackArg = { (): Promise<{}> | {} };
+
+export type optsVariable = SetOpts | undefined;
+
+export interface cacheArgs {
+  /**
+   ** resolver info object must be passed to cache function for access to query
+   */
+  info: GraphQLResolveInfo;
+  /**
+   ** expire time in seconds
+   ** use -1 to specify no expiration
+   */
+  ex?: number;
+}
+
+export type ITypedefDS =
+  | string
+  | Source
+  | DocumentNode
+  | GraphQLSchema
+  | DefinitionNode
+  | Array<ITypedefDS>
+  | (() => ITypedefDS);
+
+export interface ExecutableSchemaArgs<TContext = any> {
+  typeDefs: ITypedefDS; // type definitions used to make schema
+  resolvers?: IResolvers<any, TContext> | Array<IResolvers<any, TContext>>; // resolvers for the type definitions
+}
+
+export type Maybe<T> = null | undefined | T;
+export interface CacheKeyObj {
   readonly name: string;
   readonly arguments?: ReadonlyArray<ArgumentNode>;
-  // deno-lint-ignore no-explicit-any
   readonly variables?: Maybe<{ [key: string]: any }>;
 }
 
